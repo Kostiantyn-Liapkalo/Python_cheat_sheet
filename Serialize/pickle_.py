49d54b3f959f6e636475a2a5d29b3ad26d50b614
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-03-01 23:07:16 +0200

    pickle_

--- Serialize/pickle_.py
+++ Serialize/pickle_.py
@@ -13,19 +13,16 @@ def read_contacts_from_file(filename):
 
 
 if __name__ == "__main__":
-    filename = r""
-    contacts = [
-        {
-            "name": "Allen Raymond",
-            "email": "nulla.ante@vestibul.co.uk",
-            "phone": "(992) 914-3792",
-            "favorite": False,
-        },
-        {
-            "name": "Alisa Porter",
-            "email": "alisa2015@gmail.com",
-            "phone": "(067) 111-11-11",
-            "favorite": True,
-        }]
+    filename = r"/home/koss/Desktop/data.bin"
+    contacts = [{
+        'name': 'Allen Raymond', 
+        'email': 'nulla.ante@vestibul.co.uk', 
+        'phone': '(992) 914-3792', 
+        'favorite': False}, 
+                {
+        'name': 'Chaim Lewis', 
+        'email': 'dui.in@egetlacus.ca', 
+        'phone': '(294) 840-6685', 
+        'favorite': False}]
     write_contacts_to_file(filename, contacts)
     print(read_contacts_from_file(filename))
